09023ec72bd6fc12b6540e544ce30a8e62c656b3
maroof2424 <maroof96965@gmail.com>
2025-09-28 12:23:59 +0500

    day4

--- application/controllers/Hello.php
+++ application/controllers/Hello.php
@@ -4,10 +4,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Hello extends CI_Controller {
     public function index () {
-        echo "Hello CodeIgniter!";
+        $this->load->view('hello_view');
     }
     public function greet($name = "Guest") {
-        echo "Wellcome " . $name;
+        $data["person"] = $name;
+        $data["title"] = "Greeting page for {$name}";
+
+        $this->load->view('greet_view', $data);
+
     }
     public function sum($a, $b) {
     echo $a + $b;
---
+++ application/views/greet_view.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+    <h1>Hello, <?php echo $person; ?> 👋</h1>
+</body>
+</html>
